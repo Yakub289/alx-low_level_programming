60f55c4cff73b85b14117fd7b037ad368f05a59e
Yakub289 <yakubsuleiman24@gmail.com>
2023-02-16 15:22:03 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,7 @@
  * Return: Always 0
  */
 int main (void)
-{puts ("\"with proper grammar, but the outcome is a piece of art");
+{
+	puts("\"with proper grammar, but the outcome is a piece of art");
 	return (0);
 }
