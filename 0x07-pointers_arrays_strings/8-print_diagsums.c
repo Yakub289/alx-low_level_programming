304f2875492f30c166efcfff7b7f680683ec2848
Yakub289 <yakubsuleiman24@gmail.com>
2023-03-06 03:13:52 -0800

    The line of life is a ragged diagonal between duty and desire

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Print the sum of the diagonals of an array.
+ * @a: rows.
+ * @size: size of columns.
+ * Return: Always 0 (success)
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, row;
+	int value1, value2;
+
+	value1 = value2 = 0;
+
+	for (row = 0; row < size; row++)
+	{
+		i = (row * size) + row;
+		value1 += a[i];
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		i = (row * size) - row;
+		value2 += a[i];
+	}
+
+	printf("%d, %d\n", value1, value2);
+}
