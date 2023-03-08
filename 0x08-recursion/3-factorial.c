fd38df95463d9dbe7d0ad8f9ce2793823b80202b
Yakub289 <yakubsuleiman24@gmail.com>
2023-03-08 01:41:04 -0800

    Once an idea has taken hold of the brain it is almost impossible to eradicate

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - Compute the factorial of a number.
+ * @n: given number.
+ * Return: factorial of number n.
+ */
+
+int factorial(int n)
+{
+	if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+
+	if (n == 0)
+		return (1);
+
+	if (n < 0)
+		return (-1);
+
+	return (0);
+}
