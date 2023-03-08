6e6155440950747809145f8e3f1d39c3376f2a29
Yakub289 <yakubsuleiman24@gmail.com>
2023-03-08 01:49:53 -0800

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - Find the square root of given number.
+ * @n: given number.
+ * @square: square of number n.
+ * Return: square root or -1 if none.
+ */
+
+int root(int n, int square);
+
+int _sqrt_recursion(int n)
+{
+	int num = 0;
+	int result;
+
+	if (n > 1)
+	{
+		result = root(n, num);
+		return (result);
+	}
+
+	if (n == 1)
+		return (1);
+
+	if (n < 0)
+		return (-1);
+
+	return (0);
+}
+
+/**
+ * root - Compute the root of num from main.
+ * @n: number to get its root.
+ * @square: root of number num.
+ * Return: result of computation.
+ */
+
+int root(int n, int square)
+{
+	if (square * square == n)
+		return (square);
+
+	if (square == n / 2)
+		return (-1);
+
+	return (root(n, square + 1));
+}
