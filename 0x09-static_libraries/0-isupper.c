a6b92cfe4bc4c5995e5edbfd0d6c4dec8854236b
Yakub289 <yakubsuleiman24@gmail.com>
2023-03-10 02:21:31 -0800

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Checking for uppercase letters.
+ * @c: character to check.
+ * Return: 1 if c is uppercase, 0 if otherwise.
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - print string to stdout
+ * @str: placeholder for string
+ * Return: Always 0 (success)
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
