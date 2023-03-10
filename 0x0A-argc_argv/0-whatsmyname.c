278506649d88701313c7514ad63c0c9bce5bd61d
Yakub289 <yakubsuleiman24@gmail.com>
2023-03-10 04:26:30 -0800

    It not what they call you, it is what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point; print name of program.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
