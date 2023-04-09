64dcc8a2a0d46cde2e6e3d3c7bb9133017254647
Yakub289 <Yakubsuleiman24@gmail.com>
2023-04-09 06:09:12 -0700

    The size of the bit at the index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,4 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * get_bit - Write a function that returns a bit value in at a given index.
+ * @n: the size of bit value at the index.
+ * @index: is the starting point by 0 in bit size.
+ * Return: The value of bit at index (success), or else
+ * -1 if error occurs.
+ */
 
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
+}
