975510ac2e7490a5e4cefa647df44174a35f8d02
Yakub289 <Yakubsuleiman24@gmail.com>
2023-04-09 05:51:12 -0700

    Print binary number representation using bitwise operators

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_binary - Write a function that print binary representation of numbers.
+ * @n: number of binary representation.
+ * Return: Nil.
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n >> 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
