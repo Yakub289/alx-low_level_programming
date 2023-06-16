708a2b29573c3ff216c38424c4ebd005a8373a90
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-15 18:46:49 -0700

    Node index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -30,7 +30,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			{
 				temp->prev->next = NULL;
 			}
-			else if (index < 0)
+			else if (index > 0)
 			{
 				temp->next->prev = temp->prev;
 				temp->prev->next = temp->next;
@@ -43,7 +43,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			}
 
 			free(temp);
-			break;
 		}
 
 		node_index += 1;
