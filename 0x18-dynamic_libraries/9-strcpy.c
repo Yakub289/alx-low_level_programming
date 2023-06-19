e355d861c8e083bc71a9c024ca0e6e3d0185e49c
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-19 16:15:53 -0700

    Description, Prototype and Library

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * *_strcpy - copy strings
+ * @dest: destination of string
+ * @src: source of string
+ * Return: return pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *copied = strcpy(dest, src);
+
+	return (copied);
+}
