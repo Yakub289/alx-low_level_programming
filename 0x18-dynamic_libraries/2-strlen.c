f97e09a3bd885e480639897084e1b86503a6f91d
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-19 14:52:47 -0700

    Source files,Prototype & Description

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - find the length of provided string
+ * @s: string to be  parsed
+ * Return: return the string length
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+
+	return (length);
+}
---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * *_strstr - find the needle in the haystack
+ *
+ * @haystack: where to search
+ * @needle: what to search for
+ *
+ * Return: return pointer to first occurence
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
