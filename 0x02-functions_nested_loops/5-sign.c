57ef749f9383b0d9c9e74a5d7c3c3ba7616d48f8
Yakub289 <Yakubsuleiman24@gmail.com>
2023-07-12 06:45:12 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,19 +7,27 @@
  * 0 and prints 0 if n is zero,
  * -1 and prints - if n is less than zero.
  */
+
 int print_sign(int n)
 {
+	char positive_sign = '+';
+	char negative_sign = '-';
+	char number = '0';
+
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(positive_sign);
 		return (1);
-	} else if (n == 0)
-	{
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
 	}
+	else if (n < 0)
+	{
+		_putchar(negative_sign);
 		return (-1);
+	}
+	else
+	{
+		_putchar(number);
+		return (0);
+	}
+
 }
